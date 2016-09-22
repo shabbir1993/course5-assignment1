323517eebf0d29060cd78ef263e35ba2eb7993e3
shabbir1993 <syed_shabbir_haider@hotmail.com>
2016-09-22 22:37:28 +0500

    Protected Dependency injection after minification

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function () {
+/*(function () {
 'use strict';
 
 angular.module('DIApp', [])
@@ -20,12 +20,9 @@ function DIController($scope) {
       $scope.MessagetoUser =  "Too Much!";
     }
   };
-
-  /*function ClearsMessage(){
-    if($scope.MessagetoUser.length>0){
-      $scope.MessagetoUser = "";
-    }
-  }*/
 }
 
-})();
+})();*/
+(function(){'use strict';angular.module('DIApp',[]).controller('DIController',DIController);DIController.$inject=['$scope'];function DIController($scope){$scope.inputText="";$scope.MessagetoUser="";$scope.displayMessage=function(inputText){var items=inputText.split(',').length;if(inputText==""){$scope.MessagetoUser="Please input atleast 1 value"}
+else if(items>0&&items<=3){$scope.MessagetoUser="Enjoy!"}
+else if(items>3){$scope.MessagetoUser="Too Much!"}}}})()
